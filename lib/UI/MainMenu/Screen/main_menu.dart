a1c2000b0aa9906bc353862db2c018673d876858
Crispian <crispian4660@email.com>
2023-08-20 14:43:37 +0800

    update ButtomNavBar

--- lib/UI/MainMenu/Screen/main_menu.dart
+++ lib/UI/MainMenu/Screen/main_menu.dart
@@ -1,4 +1,3 @@
-import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hd_learning_app/UI/Home/Screen/home_screen.dart';
